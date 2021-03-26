45161133d5cc0189d91263d7e9a6e2bd9a8716e4
Theodore Evans <theodore.evans@dai-labor.de>
2021-03-26 08:37:37 +0000

    Add preprocessing to test and remove ignore_border

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import os
 import unittest
 import numpy as np
@@ -6,22 +6,36 @@ from app.inference_runner import InferenceRunner
 from data.wsi_tile_fetcher import WSITileFetcher
 from app.mock_api import MockAPI
 from nn import UNet
-import PIL.Image
+import cv2
 from PIL import Image
+from torchvision.transforms import Compose, ToTensor, Normalize
+
+TORCH_VISION_MEAN = np.asarray([0.485, 0.456, 0.406])
+TORCH_VISION_STD = np.asarray([0.229, 0.224, 0.225])
+test_transform = Compose([ToTensor(), Normalize(
+    mean=TORCH_VISION_MEAN, std=TORCH_VISION_STD)])
 
 model_path = "/model/hacking_kidney_16934_best_metric.model-384e1332.pth"
 sample_image_file = "/data/hubmap-kidney-segmentation/train/54f2eec69.tiff"
-inference = InferenceRunner(model_path)
-test_input = np.zeros((3,1024,1024))
-upper_left = (14000,6000)
+inference = InferenceRunner(model_path, data_transform=test_transform)
+upper_left = (14000, 6000)
 size_to_process = (4096, 4096)
 mock_api = MockAPI(sample_image_file, upper_left)
 tile_fetcher = WSITileFetcher(mock_api.mock_tile_request, size_to_process)
-#%%
-output = inference(tile_fetcher)
 # %%
-Image.fromarray(output)
+input_image = np.transpose(np.uint8(
+    mock_api.image_data[:, upper_left[0]:upper_left[0]+size_to_process[0], upper_left[1]:upper_left[1] + size_to_process[1]]), (1, 2, 0))
+output_mask = np.zeros_like(input_image)
+output_mask[:, :, 1] = inference(tile_fetcher)
 # %%
-Image.fromarray(np.transpose(np.uint8(mock_api.image_data[:, 14000:18096, 6000:10044]), (1,2,0)))
+Image.fromarray(output_mask)
+# %%
+
+Image.fromarray(input_image)
+
+# %%
+alpha = 0.5
+superimposed = cv2.addWeighted(input_image, alpha, output_mask, (1 - alpha), 0)
+Image.fromarray(superimposed)
 
 # %%
