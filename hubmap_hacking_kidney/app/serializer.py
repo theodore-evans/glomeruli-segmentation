29a4a67b9fcb4a7b87c641c6294eb04e53f0827e
Theodore Evans <theodore.evans@dai-labor.de>
2021-05-10 14:14:15 +0200

    added new required fields for annotation outputs

--- hubmap_hacking_kidney/app/serializer.py
+++ hubmap_hacking_kidney/app/serializer.py
@@ -8,7 +8,10 @@ def contours_to_collection(contours: List, wsi_id: str, rectangle_id: str):
             "name": f"Glomerulus {i}",
             "type": "polygon",
             "reference_id": wsi_id,  # each point annotation references my_wsi
+            "reference_type": "wsi",
             "coordinates": contour,  # Always use WSI base level coordinates
+            "npp_created": 500,
+            "npp_viewing": [499, 256000],
         }
         polygons.append(p)
 
@@ -16,6 +19,7 @@ def contours_to_collection(contours: List, wsi_id: str, rectangle_id: str):
         "item_type": "polygon",
         "items": polygons,
         "reference_id": rectangle_id,
+        "reference_type": "annotation",
     }
 
     return collection
