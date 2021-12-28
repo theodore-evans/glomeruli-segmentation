3fd8f5fc7430466422406ad5c54b8ddcd3f10a11
Theodore Evans <theodore.evans@dai-labor.de>
2021-12-28 12:30:59 +0100

    updated inputs naming

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import os
 
 from app.api_interface import ApiInterface
@@ -8,12 +7,12 @@ from app.inference_runner import InferenceRunner
 from app.output_serializer import result_to_collection
 from data.preprocessing import raw_test_transform
 from data.wsi_tile_fetcher import WSITileFetcher
+from app.logging_tools import get_logger, get_log_level
 
 DEFAULT_MODEL_PATH = "../model/hacking_kidney_16934_best_metric.model-384e1332.pth"
-VERBOSITY = 2 #Debug (temporary hard-coding)
 
-def main(model_path: str):
-    logger = logging.getLogger(__name__)
+def main(model_path: str, verbosity: int):
+    logger = get_logger(__name__, get_log_level(verbosity))
     query_parameters = dict()
 
     try:
@@ -24,19 +23,19 @@ def main(model_path: str):
         logger.error("Missing EMPAIA API environment variables")
         raise e
 
-    api = ApiInterface(verbosity=VERBOSITY, parameters=query_parameters)
+    api = ApiInterface(verbosity, query_parameters)
 
     inference_runner = InferenceRunner(model_path, data_transform=raw_test_transform())
 
 
-    my_rectangle = api.get_input("my_rectangle")
-    kidney_wsi = api.get_input("kidney_wsi")
+    my_rectangle = api.get_input("region_of_interest")
+    slide = api.get_input("kidney_wsi")
     upper_left = my_rectangle["upper_left"]
     size_to_process = (my_rectangle["width"], my_rectangle["height"])
 
 
     def tile_request(rect):
-        return api.get_wsi_tile(kidney_wsi, rect)
+        return api.get_wsi_tile(slide, rect)
 
 
     tile_fetcher = WSITileFetcher(tile_request, my_rectangle)
@@ -49,7 +48,7 @@ def main(model_path: str):
 
     api.post_output(key="glomerulus_count", data=count_result)
 
-    contour_result = result_to_collection(extracted_entities, kidney_wsi["id"], my_rectangle["id"])
+    contour_result = result_to_collection(extracted_entities, slide["id"], my_rectangle["id"])
 
     api.post_output(key="glomeruli_polygons", data=contour_result)
 
@@ -57,9 +56,10 @@ def main(model_path: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Detect glomeruli on kidney wsi")
+    
     parser.add_argument("--model", dest="model_path", default=DEFAULT_MODEL_PATH)
     parser.add_argument("-v", "--verbose", help="increase logging verbosity", action="count", default=0)
 
     args = parser.parse_args()
     
-    main(args.model)
+    main(args.model, args.verbose)
